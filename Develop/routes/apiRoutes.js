9b21ac8b792fbe9837bcd3f0330ee9cfa3c92490
Emily McLean <emilymclean93@gmail.com>
2023-04-26 19:32:57 -0400

    added post to the apiRoutes.js

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,4 +1,9 @@
 const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+
+// TODO: GET /api/notes should read the db.json file and return all saved notes as JSON.
 
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
@@ -6,3 +11,34 @@ notes.get('/', (req, res) => {
   
     readFromFile('./db.json').then((data) => res.json(JSON.parse(data)));
   });
+
+// TODO:  POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+// POST Route for submitting notes
+notes.post('/', (req, res) => {
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        note_id: uuidv4(),
+      };
+  
+      readAndAppend(newNote, './db.json');
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting new note');
+    }
+  });
+
+module.exports = notes;
