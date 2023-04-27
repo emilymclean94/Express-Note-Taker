452602f9e6b88974bbad260a9ed38c44c7a670ae
Emily McLean <emilymclean93@gmail.com>
2023-04-26 21:03:11 -0400

    added html.get to the htmlRoutes.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const api = require('./Develop/routes/index.js');
 // TODO: Change to heroku port
 const PORT = process.env.PORT || 3001;
 
+app.get("/", (req, res) => {
+  //handle root
+});
+
 const app = express();
 
 //? Import custom middleware
@@ -20,16 +24,7 @@ app.use('/api', api);
 app.use(express.static('public'));
 
 
-// TODO: GET /notes should return the notes.html file.
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'develop/public/notes.html'))
-);
 
-
-// TODO: GET * should return the index.html file.
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'develop/public/index.html'))
-);
 
 
 // TODO: Update with heroku info?
