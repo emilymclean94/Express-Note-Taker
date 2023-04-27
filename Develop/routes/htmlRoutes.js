63a64b8678d1a8fc4b80a58f2862a6e647972dfe
Emily McLean <emilymclean93@gmail.com>
2023-04-26 21:13:48 -0400

    fixed 'get started' button bug

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,15 +1,16 @@
 const html = require('express').Router();
 const path = require('path');
 
+
 // TODO: GET /notes should return the notes.html file.
 html.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'develop/public/notes.html'))
+  res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
 
 
 // TODO: GET * should return the index.html file.
 html.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'develop/public/index.html'))
+  res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
 module.exports = html;
