b04b5396bee96349cbb349146dcf38b2ace86c4d
Emily McLean <emilymclean93@gmail.com>
2023-04-28 18:08:53 -0400

    fixed pathways between html files and their styling/script

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,13 +3,14 @@ const { v4: uuidv4 } = require('../helpers/uuid');
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
 
+
 // TODO: GET /api/notes should read the db.json file and return all saved notes as JSON.
 
 // GET Route for retrieving all the notes
-notes.get('/', (req, res) => {
+notes.get('/notes', (req, res) => {
     console.info(`${req.method} request received for notes`);
   
-    readFromFile('./db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
 // TODO:  POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
